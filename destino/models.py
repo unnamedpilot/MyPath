6a503e0d5f9a0fa42582f167b513204d3bee4526
Edwar Mauricio Carrillo Carvajal <emcarrillc@eafit.edu.co>
2023-08-15 11:35:13 -0500

    minimoViable

---
+++ destino/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+
+class destino (models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=250)
+    image = models.ImageField(upload_to='destino/images/')
+    coordinates = models.CharField(max_length=50)
+    
---
+++ destino/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+from .models import destino
+
+def home(request):
+    searchTerm=request.GET.get('searchDestino')
+
+    if searchTerm:
+        destinos= destino.objects.filter(name__icontains=searchTerm)
+    else:
+        destinos=destino.objects.all()
+        
+    return render(request,'home.html',{'searchTerm':searchTerm,'destinos':destinos})
