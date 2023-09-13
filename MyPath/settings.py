57d14306e318d5728168c5bdc35a2bd49fcafc4f
MauricioCa07 <104088460+MauricioCa07@users.noreply.github.com>
2023-09-13 11:30:38 -0500

    Update settings.py

--- MyPath/settings.py
+++ MyPath/settings.py
@@ -27,7 +27,7 @@ MEDIA_URL='/media/'
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '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'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
