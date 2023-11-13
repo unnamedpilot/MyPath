7bad96f7770deb97515f23c414b0999e4ce74006
UnnamedAlt <83482674+UnnamedAlt@users.noreply.github.com>
2023-11-12 22:44:08 -0500

    Separación de preocupaciones con la app accounts

--- path_maker/models.py
+++ path_maker/models.py
@@ -1,7 +1,4 @@
 from django.db import models
 
-class user(models.Model):
-    name = models.CharField(max_length=50)
-    email =  models.CharField(max_length=25, unique=True)
-    password = models.CharField(max_length=16)
+
 
